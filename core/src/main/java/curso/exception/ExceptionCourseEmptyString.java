fea8c99b9da973329b44adb8701e3ae7e9f3aea5
maxijulio9 <maximilianojavierjulio@gmail.com>
2024-10-18 18:17:30 -0300

    Adding Util to solve the conflic

---
+++ core/src/main/java/curso/exception/ExceptionCourseEmptyString.java
@@ -0,0 +1,7 @@
+package curso.exception;
+
+public class ExceptionCourseEmptyString extends RuntimeException{
+    public ExceptionCourseEmptyString(String message){
+        super(message);
+    }
+}
