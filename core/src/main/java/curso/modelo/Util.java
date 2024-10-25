5cc2a100e091b4178eb9cc4ffccd07e89323be26
maxijulio9 <102882836+maxijulio9@users.noreply.github.com>
2024-10-25 11:00:41 -0300

    Update Util.java

--- core/src/main/java/curso/modelo/Util.java
+++ core/src/main/java/curso/modelo/Util.java
@@ -17,13 +17,11 @@ public class Util {
 
     public static void validatedateExpirationInscription(LocalDate dateExpirationInscription, String messageException) throws RuntimeException {
         if(dateExpirationInscription.isBefore(LocalDate.now()) || dateExpirationInscription == null){
-            //throw new ExceptionCursoWithAInscriptionDateInvalid("El curso que intentas registrar tiene fecha de inscripción inválida.");
             throw new ExceptionCursoWithAInscriptionDateInvalid(messageException);
         }
     }
     public static void validateLevel(CursoLevels level, String messageException) throws RuntimeException {
         if ((level != CursoLevels.INICIAL && level != CursoLevels.MEDIO && level != CursoLevels.AVANZADO) || level == null  ){
-            //throw new ExceptionCursoWithAInvalidLevel("El curso que intentas registrar tiene un nivel inválido.");
             throw new ExceptionCursoWithAInvalidLevel(messageException);
         }
     }
