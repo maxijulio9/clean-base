b2b211bca3468eb60afd8cbcf124883ce3e8c2ea
maxijulio9 <102882836+maxijulio9@users.noreply.github.com>
2024-10-25 10:55:36 -0300

    Update core/src/main/java/curso/input/ISeachForCursoByLevel.java
    Co-authored-by: Javier Ruitti <ruittijavier@gmail.com>

--- core/src/main/java/curso/input/ISeachForCursoByLevel.java
+++ core/src/main/java/curso/input/ISeachForCursoByLevel.java
@@ -6,6 +6,6 @@ import curso.modelo.CursoLevels;
 import java.util.Collection;
 import java.util.List;
 
-public interface ISeachForCursoByLevel {
+public interface ISearchForCursoByLevel {
     List<Curso> getCursoByLevel(CursoLevels level);
 }
