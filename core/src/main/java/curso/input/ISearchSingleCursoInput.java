02ffb71157a973f2270bd7b4b440b07516404f96
maxijulio9 <102882836+maxijulio9@users.noreply.github.com>
2024-10-25 10:58:44 -0300

    Update ISearchSingleCursoInput.java

--- core/src/main/java/curso/input/ISearchSingleCursoInput.java
+++ core/src/main/java/curso/input/ISearchSingleCursoInput.java
@@ -7,15 +7,6 @@ import java.util.List;
 public interface ISearchSingleCursoInput {
 
     Curso getSingleCurso(String nameCurso);
-    //Se separó por interfaces
-    //Curso searchSingleCourse(String nameCurso);
-
-//    List<Curso> searchForCoursesThatMatchAString(String criteria);
-
-  //  List<Curso> searchCoursesByLevel(CursoLevels level);
-
-    //List<Curso> searchCoursesByExpirationDateInscription(LocalDate expirationDate);
-
-  //  List<Curso> searchCoursesBetweenExpirationDateInscription(LocalDate expirationDate);
+  
 
 }
