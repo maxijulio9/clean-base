cf136f738c171891b66add14cfe1360c2dbf5409
maxijulio9 <102882836+maxijulio9@users.noreply.github.com>
2024-10-25 10:59:50 -0300

    Update Curso.java

--- core/src/main/java/curso/modelo/Curso.java
+++ core/src/main/java/curso/modelo/Curso.java
@@ -31,7 +31,6 @@ public class Curso {
     }
 
     public static Curso getInstance(String name, CursoLevels level, LocalDate dateExpirationInscription) {
-
         Util.validatedateExpirationInscription(dateExpirationInscription,"El curso que intentas registrar tiene fecha de inscripción inválida." );
         Util.validateEmptyString(name, "El nombre del curso no puede estar vacío.");
         Util.validateLevel(level, "El curso que intentas registrar tiene un nivel inválido.");
