474c94f2cd1420042ed7841cda58d02e78b8ac85
maxijulio9 <102882836+maxijulio9@users.noreply.github.com>
2024-10-25 11:02:12 -0300

    Update CursoCreateUseCase.java

--- core/src/main/java/curso/usecase/CursoCreateUseCase.java
+++ core/src/main/java/curso/usecase/CursoCreateUseCase.java
@@ -17,29 +17,11 @@ public class CursoCreateUseCase implements ICreateCursoInput {
     public CursoCreateUseCase(IPersistence myDB){
         this.myDB = myDB;
     }
-      //getInstance
-        //Persistencia validar si existe antes de crear
-        //Persistencia si no existe, agregarlo.
+  
 
     @Override
     public Curso createCurso(String name, CursoLevels level, LocalDate dateExpirationInscription) throws  RuntimeException{
-        //Curso curso = Curso.getInstance(name, level, dateExpirationInscription);
-        //implementamos Factory mejor
-        //Elimino el patrón
-        /*
-        CursoFactory cursoFactory = new CursoFactory(new CursoWithNullAttributeValidatorUseCase()
-                                                    , new CursoWithInvalidExpirationDateInscriptionUseCase()
-                                                    , new CursoWithInvalidLevelUseCase());
-
-        Curso curso = cursoFactory.createCursoFromFactory(name, level, dateExpirationInscription);
-         */
-        //Aca validaciones llevar a entidad Curso
-       /* Util.validatedateExpirationInscription(dateExpirationInscription,"El curso que intentas registrar tiene fecha de inscripción inválida." );
-        Util.validateEmptyString(name, "El nombre del curso no puede estar vacío.");
-        Util.validateLevel(level, "El curso que intentas registrar tiene un nivel inválido.");
-
-        */
-      //innecesario  Util.validateNullAtributteExistence(level, dateExpirationInscription, "Debés proporcionar todos los atributos del curso a registrar.");
+     
 
         Curso curso =  Curso.getInstance(name, level, dateExpirationInscription);
 
